3b7861901e80284cb0a91565cef5b7840c8c140c
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-08-12 14:56:43 -0300

    adds Address model

---
+++ src/app/models/address.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Address = sequelize.define(
+    'Address',     
+    {
+      street: DataTypes.STRING,
+      number: DataTypes.INTEGER,
+      district: DataTypes.STRING,
+      complement: DataTypes.TEXT,
+      zipCode: DataTypes.STRING,
+      city: DataTypes.STRING,
+      state: DataTypes.STRING,
+    }, {});
+  Address.associate = (models) => {
+    Address.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+  return Address;
+};
