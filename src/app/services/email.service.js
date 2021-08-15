5a4416cf9b0301c1e8ba1c57e5aafc18e4ca5ee0
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-08-15 17:09:06 -0300

    adds forget password email template and email service

---
+++ src/app/services/email.service.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+const { EMAIL } = require('../../config/environment');
+const forgetPasswordTemplate = require('../email/forgetPasswordTemplate');
+
+const transporter = nodemailer.createTransport({
+  host: EMAIL.smtp,
+  port: EMAIL.port,
+  secure: false,
+  service: 'gmail',
+  auth: {
+    user: EMAIL.email,
+    pass: EMAIL.password,
+  },
+});
+
+const sendForgetPasswordEmail = async (sendTo, forgetPasswordCode) => {
+  const html = forgetPasswordTemplate.makeEmail(forgetPasswordCode);
+
+  const mailOptions = {
+    from: EMAIL.email,
+    to: sendTo,
+    subject: 'Código para recuperação de senha',
+    html,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = {
+  sendForgetPasswordEmail,
+};
