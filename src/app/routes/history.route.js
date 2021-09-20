51a199e2bbd1490efbeb6946616006eff0bd1c10
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-09-20 16:17:13 -0300

    adds get all

--- src/app/routes/history.route.js
+++ src/app/routes/history.route.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const controller = require('../controllers/history.controller');
-const { verifyAuthorization, verifyHistoryAuth } = require('../middlewares/auth');
 const {
-  WHO_PATIENT,
-} = require('../util/constants');
+  verifyAuthorization,
+  verifyHistoryAuth,
+} = require('../middlewares/auth');
+const { WHO_PATIENT } = require('../util/constants');
 
 const router = express.Router();
 
-router.post('/', verifyAuthorization(WHO_PATIENT), controller.create)
-router.get('/patient/:patientId/activity/:activityId', verifyHistoryAuth, controller.getAll)
+router.post('/', verifyAuthorization(WHO_PATIENT), controller.create);
+router.get('/patient/:patientId', verifyHistoryAuth, controller.getAll);
 
-module.exports = router
+module.exports = router;
--- src/app/services/history.service.js
+++ src/app/services/history.service.js
@@ -1,7 +1,32 @@
-const { History } = require('../models')
+const { Op } = require('sequelize');
+const { History, Category, Activity } = require('../models');
 
-const create = (data) => History.create(data)
+const create = (data) => History.create(data);
+
+const getAll = ({ patientId }, date1, date2) => {
+  const query = {
+    where: {
+      patientId,
+      endTime: {
+        [Op.between]: [date1, date2],
+      },
+    },
+    include: [
+      {
+        model: Activity,
+        as: 'activity',
+        include: {
+          model: Category,
+          as: 'category',
+        },
+      },
+    ],
+  };
+
+  return History.findAll(query);
+};
 
 module.exports = {
-  create
-}
+  create,
+  getAll,
+};
